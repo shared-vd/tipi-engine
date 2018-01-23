36df27b4e31b3716938b70fbee344fe723a30cdf
Jean-Eric Cuendet <jean-eric.cuendet@vd.ch>
2018-01-23 17:50:35 +0100

    Correction of SQL native query to HQL to be Database agnostic

--- src/main/java/ch/sharedvd/tipi/engine/model/DbTopProcess.java
+++ src/main/java/ch/sharedvd/tipi/engine/model/DbTopProcess.java
@@ -1,9 +1,7 @@
 package ch.sharedvd.tipi.engine.model;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("process")
@@ -11,4 +9,10 @@ import javax.persistence.NamedQuery;
         @NamedQuery(name = "DbTopProcess.findProcessesByFqn", query = "from DbTopProcess p where p.fqn = (?1) and p.parent is null")
 })
 public class DbTopProcess extends DbSubProcess {
+
+    // Utile seulement pour la requete:
+    //    ActivityRepository.findTopProcessNamesByStateAndReqEnd
+    @OneToMany(mappedBy = "process")
+    private List<DbActivity> children;
+
 }
